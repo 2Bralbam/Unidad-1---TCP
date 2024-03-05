819e621fdb58fe4f53f3e2cfae2b61bf33847c94
LuisRivera563 <XLS_GS_563@hotmail.com>
2024-03-04 23:57:12 -0600

    Cree el Proyecto para el cliente y agregue la vista de Conexion, ademas de Agregar el controlador para cambiar las vistas

---
+++ Unidad1TCPClient/ViewModels/GaleriaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad1TCPClient.ViewModels
+{
+    public class GaleriaViewModel : INotifyPropertyChanged
+    {
+        public bool Conectado { get; set; } = false;
+        public string IP { get; set; } = "127.0.0.1";
+        void OnPropertyChanged(string Propertyname = null!)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Propertyname));
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
