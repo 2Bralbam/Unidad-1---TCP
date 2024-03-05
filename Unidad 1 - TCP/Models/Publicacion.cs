f37bdf27a1736233bc8e22bb721fda75d327e42e
2Bralbam <bralbam1@gmail.com>
2024-03-05 08:03:17 -0600

    Entidad Publicacion

--- Unidad 1 - TCP/Models/Publicacion.cs
+++ Unidad 1 - TCP/Models/Publicacion.cs
@@ -8,7 +8,8 @@ namespace Unidad_1___TCP.Models
 {
     public class Publicacion
     {
-        public List<Comentario> Comentarios { get; set; } = new();
+        public List<Comentario>? Comentarios { get; set; } = new();
         public int IdPublicacion { get; set; }
+        public MensajeDTO Mensaje { get; set; } = null!;
     }
 }
