02b97c16515f572b8786aa4517dbe1625ae67ebe
2Bralbam <bralbam1@gmail.com>
2024-03-05 00:04:57 -0600

    Iniciando Config del Server

---
+++ Unidad 1 - TCP/Models/MensajeDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad_1___TCP.Models
+{
+    public class MensajeDTO
+    {
+    }
+}
---
+++ Unidad 1 - TCP/Services/TCPServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Unidad_1___TCP.Models;
+
+namespace Unidad_1___TCP.Services
+{
+    public class TCPServer
+    {
+        TcpListener server = null!;
+        List<TcpClient> Clientes = new();
+        public event EventHandler<MensajeDTO>? MensajeRecibido;
+      
+        public TCPServer()
+        {
+            
+        }
+    }
+}
