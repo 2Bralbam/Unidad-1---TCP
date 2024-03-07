921d18b20173267025201782d344c95cdebd0eaf
LuisRivera563 <XLS_GS_563@hotmail.com>
2024-03-07 15:59:39 -0600

    Progresando con la vista de la galeria

--- Unidad1TCPClient/Services/GaleriaService.cs
+++ Unidad1TCPClient/Services/GaleriaService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Net.Sockets;
 using System.Net;
-using System.Text;
-using System.Net.Http.Json;
-
+using System.Windows;
+using System.Data;
+using Xamarin.Essentials;
+using System.IO;
+using System.Xaml;
 
 namespace Unidad1TCPClient.Services
 {
@@ -12,36 +14,83 @@ namespace Unidad1TCPClient.Services
          *  este nos proveera de metodos por defecto necesarios para hacer
          *  la conexion
          */
-        TcpClient client = null!;
+        TcpClient client = new();
         private readonly int puerto = 55555;
         public void Conectar(IPAddress ip)
         {
-            /** Si el cliente no esta conectado lo conectamos,
-             *  Esta validacion es necesaria ya que el usuario puede presionar
-             *  2 o más veces el boton para conectar el cliente al servidor
-             */
-            if (!client.Connected)
+            try
             {
-                IPEndPoint ipe = new(ip, puerto);
-                client = new();
-                client.Connect(ipe);
+                /** Esta validacion es necesaria ya que el usuario puede presionar
+                 *  2 o más veces el boton para conectar el cliente al servidor
+                 */
+                if (!client.Connected)
+                {
+                    IPEndPoint ipe = new(ip, puerto);
+                    client = new();
+                    client.Connect(ipe);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
+
         public void Desconectar()
         {
            client.Close();
         }
-        public void CargarImagenes()
-        {
-
-        }
         public void SubirImagen()
         {
+            try
+            {
+                if (VerificarConexion())
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void CargarImagenes()
+        {
+            try
+            {
+                if (VerificarConexion())
+                {
 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         public void EliminarImagen()
         {
+            try
+            {
+                if (VerificarConexion())
+                {
 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        string ConvertirImagenBase64(string imagePath)
+        {
+            byte[] imageBytes = File.ReadAllBytes(imagePath);
+            string base64String = Convert.ToBase64String(imageBytes);
+
+            return base64String;
+        }
+        bool VerificarConexion()
+        {
+            return client.GetStream()!=null;
         }
     }
 }
--- Unidad1TCPClient/ViewModels/GaleriaViewModel.cs
+++ Unidad1TCPClient/ViewModels/GaleriaViewModel.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Unidad1TCPClient.Services;
 
 namespace Unidad1TCPClient.ViewModels
 {
@@ -12,6 +17,29 @@ namespace Unidad1TCPClient.ViewModels
         public bool Conectado { get; set; } = false;
         public string IP { get; set; } = "127.0.0.1";
         public int Port { get; set; } = 55555;
+        public string vista = "";
+        public ICommand ConectarCommand { get; set; }
+        public ICommand DesconectarCommand { get; set; }
+        protected GaleriaService GaleriaService { get; set; } = new();
+
+        public GaleriaViewModel()
+        {
+            ConectarCommand = new RelayCommand(ConectarServer);
+            DesconectarCommand = new RelayCommand(DesconectarServer);
+        }
+        private void DesconectarServer()
+        {
+            GaleriaService.Desconectar();
+            Conectado = false;
+            OnPropertyChanged(nameof(Conectado));
+        }
+        void ConectarServer()
+        {
+            GaleriaService.Conectar(IPAddress.Parse(IP));
+            Conectado = true;
+            MessageBox.Show("Se ah conectado al servidor correctamente");
+            OnPropertyChanged(nameof(Conectado));
+        }
 
         void OnPropertyChanged(string Propertyname = null!)
         {
